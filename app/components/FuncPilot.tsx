fbbf006b1841e0c1e1b09abec5bc48ebb30bdb8f
Zergity <zergity@gmail.com>
2023-06-12 18:09:34 +0700

    update the text for consistency

--- app/components/FuncPilot.tsx
+++ app/components/FuncPilot.tsx
@@ -23,9 +23,7 @@ const FunctionPilot = () => {
           <div className="flex flex-col items-center justify-center text-center gap-6">
             <p className="text-heading text-[34px]">ASYMPTOTIC POWER CURVES</p>
             <p className="text-normal text-base">
-              Allows the creation of an AMM for leveraged perpetual and future
-              derivatives of any index value without the risks of liquidation or
-              under-collateralization
+              No Liquidation. Infinite Liquidity.
             </p>
           </div>
           <FunctionPilotMobile />
