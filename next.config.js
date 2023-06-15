3ffed65ebe3f4271622dbcc72d06a912e642c67c
Zergity <zergity@gmail.com>
2023-06-15 08:27:20 +0700

    export command using NODE_ENV

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-};
+const nextConfig = {}
 
-module.exports = nextConfig;
+if (process.env.NODE_ENV === "production") {
+    nextConfig.output = "export"
+}
+
+module.exports = nextConfig
