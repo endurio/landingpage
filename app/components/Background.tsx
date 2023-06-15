db17bf8d4deabf07b26c3d9ba5b7fd04dc28be87
dmanh124 <dmanh120419@gmail.com>
2023-06-15 17:15:26 +0700

    fix build for particle lib

--- app/components/Background.tsx
+++ app/components/Background.tsx
@@ -1,10 +1,7 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
-// import CELLS from "vanta/dist/vanta.cells.min";
-// import * as THREE from "three";
 import { ScrollSVG } from "../icons";
 import { loadFull } from "tsparticles";
 import Particles from "react-tsparticles";
-import particlesConfig from "../assets/particlesjs-config";
 
 function shouldShowScroll() {
   return (
@@ -15,10 +12,7 @@ function shouldShowScroll() {
 }
 
 const UniverBackground = (props) => {
-  const [vantaEffect, setVantaEffect] = useState(null);
   const [showScroll, setShowScroll] = useState(shouldShowScroll());
-
-  const vantaRef = useRef(null);
 
   const onScroll = useCallback((event) => {
     setShowScroll(shouldShowScroll());
@@ -43,57 +37,125 @@ const UniverBackground = (props) => {
       window.removeEventListener("scroll", onScroll);
     };
   }, [onScroll]);
-
-  // useEffect(() => {
-  //   if (!vantaEffect) {
-  //     setVantaEffect(
-  //       CELLS({
-  //         el: vantaRef.current,
-  //         THREE: THREE,
-  //         mouseControls: true,
-  //         touchControls: true,
-  //         gyroControls: true,
-  //         minHeight: 600.0,
-  //         minWidth: 600.0,
-  //         scale: 3.0,
-  //         scaleMobile: 3.0,
-  //         color1: "#2c0c30",
-  //         color2: "#0c1857",
-  //         backgroundColor: "#0c1857",
-  //       })
-  //     );
-  //   }
-  //   // return () => {
-  //   //   if (vantaEffect) vantaEffect.destroy();
-  //   // };
-  // }, [vantaEffect]);
-  // return (
-  //   <>
-  //     <div
-  //       ref={vantaRef}
-  //       className={`fixed flex justify-center items-center w-full h-full -z-10`}
-  //     >
-  //       {showScroll && ["", "#", "#home"].includes(props.url) && (
-  //         <div className="absolute flex flex-col justify-center items-center gap-4 w-full bottom-4">
-  //           <ScrollSVG />
-  //         </div>
-  //       )}
-  //     </div>
-  //   </>
-  // );
   return (
     <div
       id="particle-background"
       className={`background-univer fixed flex justify-center items-center w-full h-full -z-10`}
     >
-      {/* <div className="absolute flex justify-center items-center w-full h-full">
-        <img src="/bg-univer.png" alt="bg" className="w-full h-full" />
-      </div> */}
       <Particles
         id="tsparticles"
         init={particlesInit}
         loaded={particlesLoaded}
-        options={particlesConfig}
+        options={{
+          particles: {
+            number: {
+              value: 160,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: "#ffffff",
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 0,
+                color: "#000000",
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+              image: {
+                src: "img/github.svg",
+                width: 100,
+                height: 100,
+              },
+            },
+            opacity: {
+              value: 0.5,
+              random: false,
+              anim: {
+                enable: false,
+                speed: 1,
+                opacity_min: 0.1,
+                sync: false,
+              },
+            },
+            size: {
+              value: 1.5,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: false,
+              distance: 150,
+              color: "#ffffff",
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 2,
+              direction: "top",
+              random: false,
+              straight: false,
+              out_mode: "out",
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: "canvas",
+            events: {
+              onhover: {
+                enable: true,
+                mode: "repulse",
+              },
+              onclick: {
+                enable: true,
+                mode: "push",
+              },
+              resize: true,
+            },
+            modes: {
+              grab: {
+                distance: 400,
+                line_linked: {
+                  opacity: 1,
+                },
+              },
+              bubble: {
+                distance: 400,
+                size: 40,
+                duration: 2,
+                opacity: 8,
+                speed: 3,
+              },
+              repulse: {
+                distance: 200,
+                duration: 0.4,
+              },
+              push: {
+                particles_nb: 4,
+              },
+              remove: {
+                particles_nb: 2,
+              },
+            },
+          },
+          retina_detect: true,
+        }}
         height="100vh"
         width="100vw"
       ></Particles>
