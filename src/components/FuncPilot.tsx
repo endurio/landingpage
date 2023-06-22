35756d7e6c81404bd76640963c29e58081f04f18
Ngo Quang Anh <anhnq82@gmail.com>
2023-06-22 16:14:15 +0700

    add desmos-react

--- src/components/FuncPilot.tsx
+++ src/components/FuncPilot.tsx
@@ -1,6 +1,11 @@
 "use client";
 import React from "react";
 import { FunctionPilotMobile, FunctionPilotTemplate } from "../icons";
+import {
+  Expression,
+  GraphingCalculator,
+  useHelperExpression,
+} from "desmos-react";
 
 const FunctionPilot = (props) => {
   return (
@@ -13,7 +18,19 @@ const FunctionPilot = (props) => {
               No Liquidation. Infinite Liquidity.
             </p>
           </div>
-          <FunctionPilotTemplate />
+          {/* <FunctionPilotTemplate /> */}
+          <GraphingCalculator
+            attributes={{ className: "calculator" }}
+            fontSize={18}
+            keypad
+            projectorMode
+            settingsMenu={false}
+            expressionsCollapsed={true}
+          >
+            <Expression id="slider" latex="x^2" color="ORANGE"/>
+            <Expression id="fn" latex="a=3"/>
+            <Point />
+          </GraphingCalculator>
         </div>
       ) : (
         <div className="flex flex-col justify-center items-center mt-[57px] pb-[120px]">
@@ -28,6 +45,17 @@ const FunctionPilot = (props) => {
       )}
     </>
   );
+};
+
+const Point = () => {
+  const a = useHelperExpression({ latex: "a" });
+
+  let label: string;
+  if (a > 0) label = "positive x-axis";
+  else if (a < 0) label = "negative x-axis";
+  else label = "origin";
+
+  return <Expression id="point" latex="(a,0)" label={label} showLabel />;
 };
 
 export default FunctionPilot;
