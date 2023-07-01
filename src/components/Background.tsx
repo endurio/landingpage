8b0e6bba56dcfe29d098a3b39827f21d650842ae
Zergity <zergity@gmail.com>
2023-07-01 16:25:08 +0700

    remove console.log

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -19,12 +19,12 @@ const UniverBackground = (props) => {
   }, []);
 
   const particlesInit = useCallback(async (engine) => {
-    console.log(engine);
+    // console.log(engine);
     await loadFull(engine);
   }, []);
 
   const particlesLoaded = useCallback(async (container) => {
-    await console.log(container);
+    // await console.log(container);
   }, []);
 
   useEffect(() => {
