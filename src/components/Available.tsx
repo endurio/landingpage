a8c1c4ddbd33ac2aecc477a3045b0fdddab44158
Zergity <zergity@gmail.com>
2023-12-18 14:32:04 +0700

    text case

--- src/components/Available.tsx
+++ src/components/Available.tsx
@@ -8,11 +8,11 @@ const Available = (props) => {
       <div className="flex flex-col justify-center items-center pt-[80px] pb-[80px] md:pt-[120px] md:pb-[120px] gap-[56px]">
         <div className="justify-center items-center text-center">
           <p className="text-heading text-[34px] md:text-[48px]">Available on</p>
-          <p className="text-[22px] md:text-[34px]" style={{color: 'gray'}}>All EVM COMPATUBLE PLATFORMS</p>
+          <p className="text-[22px] md:text-[34px]" style={{color: 'gray'}}>All EVM compatible platforms</p>
         </div>
       
         <div className="flex flex-wrap flex-row justify-center md:justify-between items-start gap-8 max-w-[584px]">
-               <div className="relative background-card box-border flex flex-col items-center justify-center rounded-3xl gap-[26px] w-[143px] h-[169px]">
+          <div className="relative background-card box-border flex flex-col items-center justify-center rounded-3xl gap-[26px] w-[143px] h-[169px]">
             <EthereumBorder />
             <div className="absolute flex flex-col items-center justify-center w-full h-full gap-[26px]">
               <div className="flex flex-col items-center gap-4 max-w-[100px] max-h-[146px]">
