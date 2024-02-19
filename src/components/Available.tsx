a460ea88d913287f3afb5abcc1992991d4283446
Zergity <zergity@gmail.com>
2024-02-19 12:23:06 +0700

    fix overflow for available on items

--- src/components/Available.tsx
+++ src/components/Available.tsx
@@ -10,7 +10,7 @@ const Available = (props) => {
           <p className="text-[22px] md:text-[34px]" style={{color: 'gray'}}>All EVM compatible platforms</p>
         </div>
       
-        <div className="flex flex-wrap flex-row justify-center md:justify-between items-start gap-8 max-w-[584px]">
+        <div className="available-on-items flex flex-wrap flex-row justify-center md:justify-between items-start gap-8 max-w-[584px]">
           <div className="relative box-border flex flex-col items-center justify-center rounded-3xl gap-[26px] w-[143px] h-[169px]">
             <EthereumBorder />
             <div className="absolute flex flex-col items-center justify-center w-full h-full gap-[26px]">
