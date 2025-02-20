cf3a287c5027f432aa62143d15544a71d6ae5873
Zergity <zergity@gmail.com>
2025-02-20 19:45:14 +0700

    launch web app with ref

--- src/components/lib/utils.tsx
+++ src/components/lib/utils.tsx
@@ -1,10 +1,10 @@
 
 export const getLaunchURL = () => {
-  const url = `https://t.me/endurio_bot`
+  const url = `https://endur.io/app`
   const urlParams = new URLSearchParams(window.location.search);
-  const startapp = urlParams.get('startapp') || urlParams.get('ref');
-  if (startapp?.length == 42 && startapp?.startsWith('0x')) {
-    return url + `?startapp=` + startapp
+  const ref = urlParams.get('ref') || urlParams.get('startapp');
+  if (ref?.length == 42 && ref?.startsWith('0x')) {
+    return url + `?ref=` + ref
   } 
   return url
 }
